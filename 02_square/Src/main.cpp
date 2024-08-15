cbf1ab291840ccf31fcf629e0fec9f3e36315437
embeddeath <marquezhdezmiguel@gmail.com>
2024-08-15 12:51:31 -0600

    Working version of moving square.

--- 02_square/Src/main.cpp
+++ 02_square/Src/main.cpp
@@ -11,8 +11,8 @@
 #include <iostream>
 #include <string>
 #include <array>
-#include <unistd.h>
 #include "key.hpp"
+#include <SquareInScreen.hpp>
 
 /***********************************************************
     Type Definitions
@@ -22,7 +22,7 @@
     Macro Definitions
 ************************************************************/
 
-
+ 
 /***********************************************************
     Unit-Internal Function Declarations
 ************************************************************/
@@ -45,24 +45,33 @@ int main (int argc, char* argv[])
     /* Detect keystroke*/
     Key keyW('W'), keyA('A'), keyS('S'), keyD('D'); 
 
+    squareInScreen mySquare(2, 0, 0); 
 
     while(1)
     {
         if (true == keyW.detectRisingEdge())
         {
-            std::cout << "Letter W is pressed" << std::endl; 
+            //std::cout << "Letter W is pressed" << std::endl;
+            mySquare.moveUpwards(); 
         }
         if (true == keyA.detectRisingEdge())
         {
-            std::cout << "Letter A is pressed" << std::endl; 
+            //std::cout << "Letter A is pressed" << std::endl;
+            mySquare.moveLeft();
         }
         if (true == keyS.detectRisingEdge())
         {
-            std::cout << "Letter S is pressed" << std::endl; 
+            //std::cout << "Letter S is pressed" << std::endl;
+            mySquare.moveDownWards();
         }
         if (true == keyD.detectRisingEdge())
         {
-            std::cout << "Letter D is pressed" << std::endl; 
+            //std::cout << "Letter D is pressed" << std::endl;
+            mySquare.moveRight();            
         }
+
+        
+        mySquare.render(); 
+        
     }
 }
